9bd63331327536959f5bd0cf49fb5f022b828682
Adriano <adriano.cotta.ramusino@gmail.com>
2022-05-05 15:16:55 +0200

    CIAO fix

--- compute_Delta.py
+++ compute_Delta.py
@@ -5,6 +5,3 @@ vel_diff = (1.0 / vel_cell_st(:,k) - 1.0 / CTM_param.v_bar)
 ind_vel_diff = find(vel_diff < tol)
 vel_diff[ind_vel_diff] = np.zeros((ind_vel_diff.shape,ind_vel_diff.shape))
 Delta_st[:,k] = np.multiply(CTM_param.len,vel_diff)
-
-
-## CIAO
