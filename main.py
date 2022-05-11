d7470074af946e685b600adfac3b14a816655635
Adriano <adriano.cotta.ramusino@gmail.com>
2022-05-11 11:37:40 +0200

    Fixed print

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ sup.createCell(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
 sup.createCell(10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
 
 print(sup.n_cells)
+print()
 
 for cell in sup.cells:
 	cell.toString()
+	print()
