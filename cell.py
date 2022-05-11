b4514064c01f2bd8c0eb9fbc625239a23b598919
Adriano <adriano.cotta.ramusino@gmail.com>
2022-05-11 11:35:54 +0200

    Fixed init

--- cell.py
+++ cell.py
@@ -1,24 +1,15 @@
-class Cell(object):
+class Cell:
 	"""Class describing the cells of the CTM model"""
-	def __init__(self, ID, length, v, w, q, q_max, phi, phi_plus, phi_minus, s, s_big, d_big, r, rho, rho_max, beta, p, is_congested):
+	def __init__(self, ID, length, v, w, q, q_max, s, r, rho_max, beta, p):
 		self.ID = ID
 		self.length = length
 		self.v = v
 		self.w = w
 		self.q = q
 		self.q_max = q_max
-		self.phi = phi
-		self.phi_plus = phi_plus
-		self.phi_minus = phi_minus
 		self.s = s
-		self.s_big = s_big
-		self.d_big = d_big
 		self.r = r
-		self.rho = rho
 		self.rho_max = rho_max
-		self.beta = beta
-		self.p = p
-		self.is_congested = is_congested
 
 
 	def toString(self):
