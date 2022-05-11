cd9466bdb6f19b810b700ed50fb92277f7bb4651
Adriano <adriano.cotta.ramusino@gmail.com>
2022-05-11 11:36:17 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import cell as c
+import supervisor as s
+
+
+sup = s.Supervisor()
+sup.createCell(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+sup.createCell(10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
+
+print(sup.n_cells)
+
+for cell in sup.cells:
+	cell.toString()
---
+++ supervisor.py
@@ -0,0 +1,23 @@
+import cell as c
+
+class Supervisor:
+	"""Controller class for the model, represents the system at large"""
+	n_cells = 0
+	cells = []
+
+	def __init__(self):
+		pass
+
+	def computeEcal(self):
+		pass
+
+	def computeDelta(self):
+		pass
+
+	def computePhi(self):
+		pass
+		
+	def createCell(self, length, v, w, q, q_max, s, r, rho_max, beta, p):
+		cell = c.Cell(self.n_cells, length, v, w, q, q_max, s, r, rho_max, beta, p) 
+		self.cells.append(cell)
+		self.n_cells = self.n_cells + 1
