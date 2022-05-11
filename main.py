e357239216a74e320385defb2f12922987d55117
Adriano <adriano.cotta.ramusino@gmail.com>
2022-05-11 12:23:06 +0200

    Removed debugging lines

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ for cell in stretch1.cells:
 	cell.toString()
 	print()
 
-print(len(stretch1.cells))
-
 stretch2 = s.Stretch()
 stretch2.createCell(20, 21, 22, 23, 24, 25, 26, 27, 28, 29)
 stretch2.createCell(30, 31, 32, 33, 34, 35, 36, 37, 38, 39)
@@ -28,5 +26,3 @@ print()
 for cell in stretch2.cells:
 	cell.toString()
 	print()
-
-print(len(stretch2.cells))
