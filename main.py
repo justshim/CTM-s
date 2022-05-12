0b01129efef60219a5752207f07a2f1a0df55ee2
davide-unipv <62263184+davide-unipv@users.noreply.github.com>
2022-05-12 16:59:50 +0200

    commit 1

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import cell as c
 import supervisor as s
 
-
 stretch2 = s.Stretch(1)
 stretch2.createCell(20, 21, 22, 23, 24, 25, 26, 27, 28, 29)
 stretch2.createCell(30, 31, 32, 33, 34, 35, 36, 37, 38, 39)
@@ -24,16 +23,4 @@ for cell in stretch2.cells:
 	cell.toString()
 	print()
 
-#print("====================")
-
-#for station in stretch2.stations:
-#	station.toString()
-#	print()	
-
-#print("====================")
-
-#for station in stretch2.stations:
-#	for i in range(len(station.services)):
-#		station.services[i].toString()
-#		print()
-
+	
