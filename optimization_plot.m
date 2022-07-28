840873f72422c390c42da01310bbf9c73501b301
davide-unipv <davide.spalenza01@universitadipavia.it>
2022-07-28 18:48:34 +0200

    aggiunti grafici ottimizzazione al variare di beta

--- optimization_plot.m
+++ optimization_plot.m
@@ -1,22 +1,23 @@
-close all
 clearvars
+close all
 clc
-warning('off','all')
-
-%path ='C:\Users\adria\Documents\Uni\LM II anno\Tesi\opti_data.xlsx';
-path = 'H:\Il mio Drive\Tesi magistrale\Python\CTM-s\opti_data.xls';
-%path ='C:/A_Tesi/Python/CTM-s/opti_data.xls';
-
+addpath(pwd);
+path=strcat(pwd,"/opti_data.xls");
+%warning('off','all')
 disp = 1;
+
 %% extract data
+
 T = readtable(path);
+
+varname=T.Properties.VariableNames;
 A = table2array(T);
-cell_in = A(:, 1);
-cell_out = A(:, 2);
-delta = A(:, 3);
-integral = A(:, 4);
-max_delta = A(:,5);
-pi_greco = A(:, 6);
+% cell_in = A(:, 1);
+% cell_out = A(:, 2);
+% delta = A(:, 3);
+% integral = A(:, 4);
+% max_delta = A(:,5);
+% pi_greco = A(:, 6);
 fit_name=["poly11","poly12","poly21","poly22","poly13","poly31","poly23","poly32","poly33", ...
           "poly14","poly41","poly24","poly42","poly34","poly43","poly44",...
           "poly15","poly51","poly25","poly52","poly35","poly53","poly45","poly54","poly55"];
@@ -28,21 +29,21 @@ y = [];
 z = [];
 for i=1:length(A(:,1))
     if(A(i,3)==delta)
-        x=[x A(i,1)];
-        y=[y A(i,2)];
-        z=[z A(i,4)];
+        x=[x A(i,1)]; %i
+        y=[y A(i,2)]; %j
+        z=[z A(i,5)]; %integral
     end
 end
 x=x'; 
 y=y'; 
 z=z';
 
-[f, gof] = fit([x, y],z,fit_name(1),'Exclude', z < 500);
+[f, gof] = fit([x, y],z,fit_name(1),'Exclude', z < 100);
 f_best=f;
 gof_best=gof;
 round_adjrsq = round(gof_best.adjrsquare,2);
 for i=2:length(fit_name)
-    [f, gof] = fit([x, y],z,fit_name(i), 'Exclude', z < 500);
+    [f, gof] = fit([x, y],z,fit_name(i), 'Exclude', z < 100);
     if(round(gof.adjrsquare,2) > round_adjrsq)
         f_best=f;
         gof_best=gof;
@@ -52,7 +53,7 @@ end
 
 if(disp>0)
     figure()
-    plot(f_best,[x y],z, 'Exclude', z < 500)
+    plot(f_best,[x y],z, 'Exclude', z < 100)
     zlim([0 3500]);
     xlabel('x - Cell IN')
     ylabel('y - Cell OUT')
@@ -63,27 +64,27 @@ if(disp>0)
 end
 
 %% Integral with different i, delta(10-120 min) and j fixed
-j = 3;
+j = 2;
 x = [];
 y = [];
 z = [];
 for i=1:length(A(:,1))
     if(A(i,2)==A(i,1)+j)
-        x=[x A(i,1)];
-        y=[y A(i,3)];
-        z=[z A(i,4)];
+        x=[x A(i,1)]; %i
+        y=[y A(i,3)]; %delta
+        z=[z A(i,5)]; %integral
     end
 end
 x=x'; 
 y=y'; 
 z=z';
 
-[f, gof] = fit([x, y],z,fit_name(1));
+[f, gof] = fit([x, y],z,fit_name(1), 'Exclude', z < 100);
 f_best=f;
 gof_best=gof;
 round_adjrsq = round(gof_best.adjrsquare,2);
 for i=2:length(fit_name)
-    [f, gof] = fit([x, y],z,fit_name(i));
+    [f, gof] = fit([x, y],z,fit_name(i), 'Exclude', z < 100);
     a = round(gof.adjrsquare,2);
     if(a > round_adjrsq)
         f_best=f;
@@ -94,7 +95,7 @@ end
 
 if(disp>0)
     figure()
-    plot(f_best,[x y],z)
+    plot(f_best,[x y],z,'Exclude', z < 100)
     zlim([0 3500]);
     xlabel('x - Cell IN')
     ylabel('y - Delta (timesteps)')
@@ -106,15 +107,15 @@ end
 
 
 %% Pi with different i, delta(10-120 min) and j fixed
-j = 3;
+j = 2;
 x = [];
 y = [];
 z = [];
 for i=1:length(A(:,1))
     if(A(i,2)==A(i,1)+j)
-        x=[x A(i,1)];
-        y=[y A(i,3)];
-        z=[z A(i,6)];
+        x=[x A(i,1)]; %i
+        y=[y A(i,3)]; %delta
+        z=[z A(i,7)]; %pi
     end
 end
 x=x'; 
@@ -143,6 +144,172 @@ if(disp>0)
     ylabel('y - Delta (timesteps)')
     zlabel("PI")
     title("PI VS i, delta")
+    legend(type(f_best))
+    grid on
+end
+
+%% Integral with different i, beta and j fixed
+j = 2;
+x = [];
+y = [];
+z = [];
+for i=1:length(A(:,1))
+    if(A(i,2)==A(i,1)+j)
+        x=[x A(i,1)]; %i
+        y=[y A(i,4)]; %beta
+        z=[z A(i,5)]; %integral
+    end
+end
+x=x'; 
+y=y'; 
+z=z';
+
+[f, gof] = fit([x, y],z,fit_name(1), 'Exclude', z < 100);
+f_best=f;
+gof_best=gof;
+round_adjrsq = round(gof_best.adjrsquare,2);
+for i=2:length(fit_name)
+    [f, gof] = fit([x, y],z,fit_name(i), 'Exclude', z < 100);
+    a = round(gof.adjrsquare,2);
+    if(a > round_adjrsq)
+        f_best=f;
+        gof_best=gof;
+        round_adjrsq = round(gof_best.adjrsquare,2);
+    end
+end
+
+if(disp>0)
+    figure()
+    plot(f_best,[x y],z, 'Exclude', z < 100)
+    zlim([0 4000]);
+    xlabel('x - Cell IN')
+    ylabel('y - Beta')
+    zlabel("Integral")
+    title("Integral VS i, beta")
+    legend(type(f_best))
+    grid on
+end
+%% Pi with different i, beta and j fixed
+j = 2;
+x = [];
+y = [];
+z = [];
+for i=1:length(A(:,1))
+    if(A(i,2)==A(i,1)+j)
+        x=[x A(i,1)]; %i
+        y=[y A(i,4)]; %beta
+        z=[z A(i,7)]; %pi
+    end
+end
+x=x'; 
+y=y'; 
+z=z';
+
+[f, gof] = fit([x, y],z,fit_name(1));
+f_best=f;
+gof_best=gof;
+round_adjrsq = round(gof_best.adjrsquare,2);
+for i=2:length(fit_name)
+    [f, gof] = fit([x, y],z,fit_name(i));
+    a = round(gof.adjrsquare,2);
+    if(a > round_adjrsq)
+        f_best=f;
+        gof_best=gof;
+        round_adjrsq = round(gof_best.adjrsquare,2);
+    end
+end
+
+if(disp>0)
+    figure()
+    plot(f_best,[x y],z, 'Exclude', z < 0)
+    zlim([-1 1]);
+    xlabel('x - Cell IN')
+    ylabel('y - Beta')
+    zlabel("PI")
+    title("PI VS i, beta")
+    legend(type(f_best))
+    grid on
+end
+
+%% Pi with different delta, beta
+
+x = [];
+y = [];
+z = [];
+for i=1:length(A(:,1))
+    if(true)
+        x=[x A(i,3)]; %delta
+        y=[y A(i,4)]; %beta
+        z=[z A(i,7)]; %pi
+    end
+end
+x=x'; 
+y=y'; 
+z=z';
+
+[f, gof] = fit([x, y],z,fit_name(1));
+f_best=f;
+gof_best=gof;
+round_adjrsq = round(gof_best.adjrsquare,2);
+for i=2:length(fit_name)
+    [f, gof] = fit([x, y],z,fit_name(i));
+    a = round(gof.adjrsquare,2);
+    if(a > round_adjrsq)
+        f_best=f;
+        gof_best=gof;
+        round_adjrsq = round(gof_best.adjrsquare,2);
+    end
+end
+
+if(disp>0)
+    figure()
+    plot(f_best,[x y],z, 'Exclude', z < 0)
+    zlim([-1 1]);
+    xlabel('x - Cell IN')
+    ylabel('y - Beta')
+    zlabel("PI")
+    title("PI VS i, beta")
+    legend(type(f_best))
+    grid on
+end
+%% Integral with different delta, beta 
+
+x = [];
+y = [];
+z = [];
+for i=1:length(A(:,1))
+    if(true)
+        x=[x A(i,3)]; %delta
+        y=[y A(i,4)]; %beta
+        z=[z A(i,5)]; %integral
+    end
+end
+x=x'; 
+y=y'; 
+z=z';
+
+[f, gof] = fit([x, y],z,fit_name(1), 'Exclude', z < 100);
+f_best=f;
+gof_best=gof;
+round_adjrsq = round(gof_best.adjrsquare,2);
+for i=2:length(fit_name)
+    [f, gof] = fit([x, y],z,fit_name(i), 'Exclude', z < 100);
+    a = round(gof.adjrsquare,2);
+    if(a > round_adjrsq)
+        f_best=f;
+        gof_best=gof;
+        round_adjrsq = round(gof_best.adjrsquare,2);
+    end
+end
+
+if(disp>0)
+    figure()
+    plot(f_best,[x y],z, 'Exclude', z < 100)
+    zlim([0 4000]);
+    xlabel('x - Cell IN')
+    ylabel('y - Beta')
+    zlabel("Integral")
+    title("Integral VS i, beta")
     legend(type(f_best))
     grid on
 end
