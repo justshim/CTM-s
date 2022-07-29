604a44636a3c67ba30e6666bd46a51cbecd36921
davide-unipv <davide.spalenza01@universitadipavia.it>
2022-07-29 17:27:28 +0200

    modificata scrittura file su csv

--- main_opti_parallel.py
+++ main_opti_parallel.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import itertools
 import multiprocessing
+import os
+import csv
 
 def mia_funz(params):
 	
@@ -21,6 +23,7 @@ def mia_funz(params):
 	global w
 	global q_max
 	global rho_max
+	beta=params[3]/100
 	###################################################
 	# Initialization of all components of the model:  #
 	###################################################
@@ -39,7 +42,7 @@ def mia_funz(params):
 				
 	## create the stations via the factory
 				#ID stretch, r_s_max, i, j, delta, beta_s, p
-	fac.addStationToStretch(0, 500, params[0], params[1], params[2], 0.07, 0.05) 
+	fac.addStationToStretch(0, 500, params[0], params[1], params[2], beta, 0.05) 
 
 	## create the on-ramps via the factory
 				#ID stretch, d_r, r_r_max, j, p_r
@@ -62,35 +65,39 @@ def mia_funz(params):
 	
 	delta_max = max(d)
 	pi = (delta_max0-delta_max)/delta_max0
-	tupla=[params[0], params[1], params[2], integ, delta_max, pi]
+	tupla=[params[0], params[1], params[2], beta, integ, delta_max, pi]
 	return tupla
 
 def main():
 	a = range(1, sh.nrows-1, 1)
 	b = range(1, sh.nrows, 1)
-	c = range(60,721, 60)
-	d = range(10)
-	paramlist = list(itertools.product(a,b,c)) #all possible combinations of tuples
+	c = range(60,61, 60)
+	d = range(1, 2, 1)
+
+	paramlist = list(itertools.product(a,b,c,d)) #all possible combinations of tuples
 	
 	paramlist2 = []
 	for i in range(len(paramlist)):
 		if(paramlist[i][1] > paramlist[i][0]): #only if exit station > enter station
 			paramlist2.append(paramlist[i])
-
+	print("Total cases to be evaluated: " + str(len(paramlist2)))
 	pool = multiprocessing.Pool() #generate processes equal to the number of cores
+	chuncksize=int(len(paramlist2)/os.cpu_count())
 	res = pool.map(mia_funz,paramlist2) #distribute the parameter sets evenly across the cores
+	#pool.join()
+	
+	print("Done!")
 	return res 
 	
 #######################
 # File read section:  #
 #######################
+
 #path_file_output = 'C:/Users/adria/Documents/Uni/LM II anno/Tesi/python/opti_data.xls'
 #path_file_output = 'C:/A_Tesi/Python/CTM-s/opti_data.xls'
-path_file_output = ("H:/Il mio Drive/Tesi magistrale/Python/CTM-s/opti_data.xls")
-wbt = xlwt.Workbook()
-out_file =  wbt.add_sheet('Sheet1')
+path_file_output = (os.getcwd()+"/opti_data.csv")
+print(os.getcwd())
 ## read file CTM_data from xls file
-
 #loc = ("C:/A_Tesi/Python/CTM-s/CTM_data.xls")
 #loc = ("C:/A_Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out.xls")
 loc = ("H:/Il mio Drive/Tesi magistrale/CTMs-identification/fnc/extracted_data/CTM_param_out.xls")
@@ -158,22 +165,12 @@ while k<duration: #execution of the first simulation without any station
 delta_max0 = max(d0)
 if __name__ == "__main__":    
 	res=main()
+	print("Writing output file... ")
 	
-	#Writing output file
-	out_file.write(0, 0, "i")
-	out_file.write(0, 1, "j")
-	out_file.write(0, 2, "delta")
-	#out_file.write(0, 3, "beta")
-	out_file.write(0, 3, "integral")
-	out_file.write(0, 4, "max_delta")
-	out_file.write(0, 5, "pi")
-
-	for i in range(1, len(res)):
-		out_file.write(i, 0, res[i][0])
-		out_file.write(i, 1, res[i][1])
-		out_file.write(i, 2, res[i][2])
-		out_file.write(i, 3, res[i][3])
-		out_file.write(i, 4, res[i][4])
-		out_file.write(i, 5, res[i][5])
-
-	wbt.save(path_file_output)
+	with open(path_file_output, 'w', encoding='UTF8', newline='') as f:
+		writer = csv.writer(f)
+		header =["i","j","delta","beta","integral","max_delta","pi"]
+		writer.writerow(header)
+		for i in range(0, len(res)):
+			writer.writerow(res[i])
+	print("Done!")
