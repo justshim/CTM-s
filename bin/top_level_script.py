d0c8944189dd74283d4a373e55663f92b66bd680
davide-unipv <62263184+davide-unipv@users.noreply.github.com>
2022-08-13 15:42:33 +0200

    polyfitn

--- bin/top_level_script.py
+++ bin/top_level_script.py
@@ -4,8 +4,8 @@ from model.optimization import data_creation as opt
 
 if __name__ == '__main__':
     # loc = ("H:/Il mio Drive/Tesi magistrale/CTMs-identification/fnc/extracted_data/CTM_param_out.xls")
-    # loc = ("C:/A_Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out.xls")
-    loc = "C:/Users/adria/Documents/Uni/LM II anno/Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out.xls"
+    loc = ("C:/A_Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out.xls")
+    # loc = "C:/Users/adria/Documents/Uni/LM II anno/Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out.xls"
     path_file_output = "../data/opti_data.csv"
     duration = 8640  # k=24h=8640 , k=1h=360, k=3h=1080
     t = time.time()
