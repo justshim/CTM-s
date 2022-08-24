bec41d096ce1f9a3e564a1d2c708a29df2e88190
davide-unipv <davide.spalenza01@universitadipavia.it>
2022-08-24 16:19:20 +0200

    scrittura csv

--- bin/ga_integral.py
+++ bin/ga_integral.py
@@ -4,6 +4,7 @@ import numpy
 
 from model import main_ga
 import pygad
+import csv
 
 
 if __name__ == '__main__':
@@ -13,8 +14,14 @@ if __name__ == '__main__':
     # path = ("C:/A_Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out_nice.xls")
     #path = "C:/Users/adria/Documents/Uni/LM II anno/Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out_nice.xls"
 
-    path_file_output = "../data/ga_result.csv"
+    path_file_output = "../data/ga_integral_result.csv"
 
+
+    # with open(path_file_output, 'w', encoding='UTF8', newline='') as f:
+    #     writer = csv.writer(f)
+    #     header = ["i", "j", "delta", "beta", "priority", "integral", "max_delta", "pi"]
+    #     writer.writerow(header)
+    # f.close()
     duration = 8640  # k=24h=8640 , k=1h=360, k=3h=1080
     #onramps = [[1000, 500, 4, 0.05]]
     #offramps = [[8, 0.05]]
@@ -33,7 +40,7 @@ if __name__ == '__main__':
 
     function_inputs = [station[0], station[1], station[2], station[3]]
 
-    num_generations = 20
+    num_generations = 1
     num_parents_mating = 4
 
     sol_per_pop = 16
@@ -53,30 +60,39 @@ if __name__ == '__main__':
     mutation_by_replacement = True
 #    initial_population = [[1, 2, 300, 0.1], [3, 6, 120, 0.05], [7, 9, 720, 0.2], [11, 12, 50, 0.02]]
     initial_population = None
+    gen=0
+    with open(path_file_output, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f, delimiter=';')
+        header = ["i", "j", "delta","beta", "integral", "pi", "fitness"]
+        writer.writerow(header)
+        def fitness_func(solution, solution_idx):
+            if(solution_idx == 0):
+                global gen
+                gen = gen + 1
+                print("\n")
+                print("************************************")
+                print("gen: " + str(gen))
+                print("************************************")
+                print("\n")
+            output = [0, 0]
+            if solution[0] < solution[1]:
+                output = main_ga.ga(path, duration, rsmax, p, solution, onramps, offramps)
+                fitness = 10000000 / output[0]
 
-    def fitness_func(solution, solution_idx):
-        if(solution_idx == 0):
-            print("\n")
-            print("************************************")
-            print("cambio popolazione")
-            print("************************************")
-            print("\n")
-        output = [0, 0]
-        if solution[0] < solution[1]:
-            output = main_ga.ga(path, duration, rsmax, p, solution, onramps, offramps)
-            fitness = 10000000 / output[0]
-            print("Solution: [i: " + str(solution[0]) + ", j: " + str(solution[1]) +
-                  ", delta: " + str(solution[2]) + ", beta: " + str(solution[3]) + "]\n" +
-                  "Fitness: " + str(fitness))
-        else:
-            fitness = 0
-            print("Invalid solution: i>j")
-        return fitness
+                print("Solution: [i: " + str(solution[0]) + ", j: " + str(solution[1]) +
+                      ", delta: " + str(solution[2]) + ", beta: " + str(solution[3]) + "]\n" +
+                      "Fitness: " + str(fitness))
+
+                writer.writerow([solution[0], solution[1], solution[2], solution[3], output[0], output[1], fitness])
+            else:
+                fitness = 0
+                print("Invalid solution: i>j")
+            return fitness
 
 
-    fitness_function = fitness_func
+        fitness_function = fitness_func
 
-    ga_instance = pygad.GA(num_generations=num_generations,
+        ga_instance = pygad.GA(num_generations=num_generations,
                            num_parents_mating=num_parents_mating,
                            fitness_func=fitness_function,
                            sol_per_pop=sol_per_pop,
@@ -93,18 +109,19 @@ if __name__ == '__main__':
                            crossover_type=crossover_type,
                            mutation_type=mutation_type,
                            mutation_num_genes=mutation_num_genes,
-                         #  mutation_percent_genes=mutation_percent_genes,
-                           parallel_processing=parallel_processing)
+                           #  mutation_percent_genes=mutation_percent_genes,
+                           # parallel_processing=parallel_processing
+                           )
 
-    ga_instance.run()
+        ga_instance.run()
 
-    solution, solution_fitness, solution_idx = ga_instance.best_solution()
-    print("Parameters of the best solution : {solution}".format(solution=solution))
-    print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+        solution, solution_fitness, solution_idx = ga_instance.best_solution()
+        print("Parameters of the best solution : {solution}".format(solution=solution))
+        print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
 
-    prediction = numpy.sum(numpy.array(function_inputs) * solution)
-    print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
+        prediction = numpy.sum(numpy.array(function_inputs) * solution)
+        print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
 
-    elapsed = time.time() - t
-    print("Elapsed time: " + str(elapsed))
-    ga_instance.plot_fitness(title="Fitness across generations")
+        elapsed = time.time() - t
+        print("Elapsed time: " + str(elapsed))
+        ga_instance.plot_fitness(title="Fitness across generations")
