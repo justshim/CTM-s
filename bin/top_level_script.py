fd9d4026449efad8f077e81604f66e476893ec6c
davide-unipv <davide.spalenza01@universitadipavia.it>
2022-08-24 15:27:40 +0200

    tuning

--- bin/top_level_script.py
+++ bin/top_level_script.py
@@ -26,60 +26,67 @@ import pygad
 if __name__ == '__main__':
     t = time.time()
 
-    # path = ("H:/Il mio Drive/Tesi magistrale/CTMs-identification/fnc/extracted_data/CTM_param_out_nice.xls")
+    path = ("H:/Il mio Drive/Tesi magistrale/CTMs-identification/fnc/extracted_data/CTM_param_out_nice.xls")
     # path = ("C:/A_Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out_nice.xls")
-    path = "C:/Users/adria/Documents/Uni/LM II anno/Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out_nice.xls"
+    #path = "C:/Users/adria/Documents/Uni/LM II anno/Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out_nice.xls"
 
     path_file_output = "../data/ga_result.csv"
 
     duration = 8640  # k=24h=8640 , k=1h=360, k=3h=1080
-    onramps = [[1000, 500, 4, 0.05]]
-    offramps = [[8, 0.05]]
-    rsmax = 500
+    #onramps = [[1000, 500, 4, 0.05]]
+    #offramps = [[8, 0.05]]
+    rsmax = 1500
     p = 0.05
-    # onramps = []
-    # offramps = []
+    onramps = []
+    offramps = []
 
     station = [1, 1, 1, 1]
 
     gene_type = [int, int, int, float]
     gene_space = [{'low': 1, 'high': 12}, {'low': 2, 'high': 13},
-                  {'low': 60, 'high': 720}, {'low': 0, 'high': 0.2}]
+                  {'low': 1, 'high': 720}, {'low': 0, 'high': 0.2}]
 
-    parallel_processing = 4
+    parallel_processing = 12
 
     function_inputs = [station[0], station[1], station[2], station[3]]
-    desired_output = 0
 
     num_generations = 20
     num_parents_mating = 4
 
-    sol_per_pop = 8
+    sol_per_pop = 16
     num_genes = len(function_inputs)
 
-    init_range_low = -2
-    init_range_high = 5
+    init_range_low = 0
+    init_range_high = 720
 
-    parent_selection_type = "rank"
-    keep_parents = 0
+    parent_selection_type = "sss"
+    keep_parents = -1
 
-    crossover_type = "single_point"
+    crossover_type = "two_points"
+    crossover_probability = 0.5
 
     mutation_type = "random"
-    mutation_percent_genes = 50
+    mutation_num_genes = 1
+    mutation_by_replacement = True
 #    initial_population = [[1, 2, 300, 0.1], [3, 6, 120, 0.05], [7, 9, 720, 0.2], [11, 12, 50, 0.02]]
-
+    initial_population = None
 
     def fitness_func(solution, solution_idx):
+        if(solution_idx == 0):
+            print("\n")
+            print("************************************")
+            print("cambio popolazione")
+            print("************************************")
+            print("\n")
         output = [0, 0]
         if solution[0] < solution[1]:
             output = main_ga.ga(path, duration, rsmax, p, solution, onramps, offramps)
-            fitness = 10000000 / numpy.abs(output[0] - desired_output)
+            fitness = 10000000 / output[0]
             print("Solution: [i: " + str(solution[0]) + ", j: " + str(solution[1]) +
                   ", delta: " + str(solution[2]) + ", beta: " + str(solution[3]) + "]\n" +
                   "Fitness: " + str(fitness))
         else:
-            fitness = -99999999
+            fitness = 0
             print("Invalid solution: i>j")
         return fitness
 
@@ -95,12 +102,15 @@ if __name__ == '__main__':
                            gene_space=gene_space,
                            init_range_low=init_range_low,
                            init_range_high=init_range_high,
-                           #initial_population=initial_population,
+                           mutation_by_replacement=mutation_by_replacement,
+                           initial_population=initial_population,
+                           crossover_probability=crossover_probability,
                            parent_selection_type=parent_selection_type,
                            keep_parents=keep_parents,
                            crossover_type=crossover_type,
                            mutation_type=mutation_type,
-                           mutation_percent_genes=mutation_percent_genes,
+                           mutation_num_genes=mutation_num_genes,
+                         #  mutation_percent_genes=mutation_percent_genes,
                            parallel_processing=parallel_processing)
 
     ga_instance.run()
