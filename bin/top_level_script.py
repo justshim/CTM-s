1f7cf0e65e3278a129b00dfcfbd69e9a94c924e3
Adriano <adriano.cotta.ramusino@gmail.com>
2022-08-24 14:19:36 +0200

    Small fixes

--- bin/top_level_script.py
+++ bin/top_level_script.py
@@ -26,27 +26,29 @@ import pygad
 if __name__ == '__main__':
     t = time.time()
 
-    path = ("H:/Il mio Drive/Tesi magistrale/CTMs-identification/fnc/extracted_data/CTM_param_out_nice.xls")
+    # path = ("H:/Il mio Drive/Tesi magistrale/CTMs-identification/fnc/extracted_data/CTM_param_out_nice.xls")
     # path = ("C:/A_Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out_nice.xls")
-    # path = "C:/Users/adria/Documents/Uni/LM II anno/Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out_nice.xls"
+    path = "C:/Users/adria/Documents/Uni/LM II anno/Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out_nice.xls"
 
     path_file_output = "../data/ga_result.csv"
 
     duration = 8640  # k=24h=8640 , k=1h=360, k=3h=1080
     onramps = [[1000, 500, 4, 0.05]]
     offramps = [[8, 0.05]]
+    rsmax = 500
+    p = 0.05
     # onramps = []
     # offramps = []
 
-    station = [1, 1, 1, 1, 1, 1]
+    station = [1, 1, 1, 1]
 
-    gene_type = [int, int, int, int, float, float]
-    gene_space = [{'low': 1500, 'high': 1500}, {'low': 1, 'high': 12}, {'low': 2, 'high': 13},
-                  {'low': 60, 'high': 720}, {'low': 0, 'high': 0.2}, {'low': 0.05, 'high': 0.05}]
+    gene_type = [int, int, int, float]
+    gene_space = [{'low': 1, 'high': 12}, {'low': 2, 'high': 13},
+                  {'low': 60, 'high': 720}, {'low': 0, 'high': 0.2}]
 
-    parallel_processing = 12
+    parallel_processing = 4
 
-    function_inputs = [station[0], station[1], station[2], station[3], station[4], station[5]]
+    function_inputs = [station[0], station[1], station[2], station[3]]
     desired_output = 0
 
     num_generations = 20
@@ -65,20 +67,20 @@ if __name__ == '__main__':
 
     mutation_type = "random"
     mutation_percent_genes = 50
-#    initial_population = [[500, 1, 2, 500, 0.1, 0.05], [500, 2, 3, 500, 0.2, 0.05], [500, 1, 2, 500, 0.1, 0.05], [500, 2, 3, 500, 0.2, 0.05]]
+#    initial_population = [[1, 2, 300, 0.1], [3, 6, 120, 0.05], [7, 9, 720, 0.2], [11, 12, 50, 0.02]]
 
 
     def fitness_func(solution, solution_idx):
         output = [0, 0]
-        if solution[1] < solution[2]:
-            output = main_ga.ga(path, duration, solution, onramps, offramps)
+        if solution[0] < solution[1]:
+            output = main_ga.ga(path, duration, rsmax, p, solution, onramps, offramps)
             fitness = 10000000 / numpy.abs(output[0] - desired_output)
-            print("Solution: [rs_max: " + str(solution[0]) + ", i: " + str(solution[1]) + ", j: " + str(solution[2]) +
-                  ", delta: " + str(solution[3]) + ", beta: " + str(solution[4]) + ", p: " + str(solution[5]) + "]\n" +
+            print("Solution: [i: " + str(solution[0]) + ", j: " + str(solution[1]) +
+                  ", delta: " + str(solution[2]) + ", beta: " + str(solution[3]) + "]\n" +
                   "Fitness: " + str(fitness))
         else:
             fitness = -99999999
-            print("Invalid solution mi vergogno sono un incapace cane randagio")
+            print("Invalid solution: i>j")
         return fitness
 
 
@@ -112,4 +114,4 @@ if __name__ == '__main__':
 
     elapsed = time.time() - t
     print("Elapsed time: " + str(elapsed))
-    ga_instance.plot_fitness(title="pino")
+    ga_instance.plot_fitness(title="Fitness across generations")
--- model/main_ga.py
+++ model/main_ga.py
@@ -2,7 +2,7 @@ from model import factory as f
 import xlrd
 
 
-def ga(path, duration, station, onramps, offramps):
+def ga(path, duration, rsmax, p, station, onramps, offramps):
     #station = [rsmax, ii, jj, delta, beta, p]
 
     # File read
@@ -53,8 +53,8 @@ def ga(path, duration, station, onramps, offramps):
         fac1.addCellToStretch(0, sh.cell_value(i, 1), sh.cell_value(i, 2), sh.cell_value(i, 3),
                               sh.cell_value(i, 4), sh.cell_value(i, 5), 1)
 
-    fac1.stretches[0].cells[station[1]].p_ms = fac1.stretches[0].cells[station[1]].p_ms - station[5]
-    fac1.addStationToStretch(0, station[0], station[1], station[2], station[3], station[4], station[5])
+    fac1.stretches[0].cells[station[1]].p_ms = fac1.stretches[0].cells[station[1]].p_ms - p
+    fac1.addStationToStretch(0, rsmax, station[0], station[1], station[2], station[3], p)
 
     if len(onramps) > 0:
         # create the on-ramps via the factory
