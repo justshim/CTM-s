3eb9175d7cdd54b2734279e7d0e727d0f0715d8b
davide-unipv <davide.spalenza01@universitadipavia.it>
2022-08-31 12:20:35 +0200

    debug reti neurali

--- model/optimization/neural_network.py
+++ model/optimization/neural_network.py
@@ -15,15 +15,20 @@ TRAIN_DATA_PATH = 'C:/Users/adria/Documents/Uni/LM II anno/Tesi/python/' \
                   'CTM-s/model/optimization/matlab/data models/4 vars/ide_set.csv'
 TEST_DATA_PATH = 'C:/Users/adria/Documents/Uni/LM II anno/Tesi/python/' \
                  'CTM-s/model/optimization/matlab/data models//4 vars/val_set.csv'
+TRAIN_DATA_PATH = './matlab/data models/4 vars/ide_set.csv'
+TEST_DATA_PATH = './matlab/data models//4 vars/val_set.csv'
+
 TARGET_NAME = 'integral'
 
 # x_train = features, y_train = target
 train_data = pd.read_csv(TRAIN_DATA_PATH)
 train_data = train_data.drop('priority', axis=1)
 train_data = train_data.drop('max_delta', axis=1)
+train_data = train_data.drop('pi', axis=1)
 
 test_data = pd.read_csv(TEST_DATA_PATH)
 test_data = test_data.drop('priority', axis=1)
+test_data = test_data.drop('pi', axis=1)
 test_data = test_data.drop('max_delta', axis=1)
 
 x_train, y_train = train_data.drop(TARGET_NAME, axis=1), train_data[TARGET_NAME]
@@ -38,10 +43,21 @@ def scale_datasets(x_train, x_test):
         standard_scaler.fit_transform(x_train),
         columns=x_train.columns
     )
+    print("mean: " + str(standard_scaler.mean_))
+    print("var: " + str(standard_scaler.var_))
+    print(x_train_scaled)
+    data_orig = standard_scaler.inverse_transform(x_train_scaled)
+    print(data_orig)
+
+
     x_test_scaled = pd.DataFrame(
-        standard_scaler.transform(x_test),
+        standard_scaler.fit_transform(x_test),
         columns=x_test.columns
     )
+    #print("mean: " + str(standard_scaler.mean_))
+    #print("var: " + str(standard_scaler.var_))
+
+
     return x_train_scaled, x_test_scaled
 
 
@@ -103,8 +119,29 @@ def plot_history(history, key):
 # Plot the history
 plot_history(history, 'mean_squared_logarithmic_error')
 
-x_test['prediction'] = model.predict(x_test_scaled)
-aaa = model(tf.constant([1, 3, 0.1, 500]))
+# x_test['prediction'] = model.predict(x_test_scaled)
+
+
+
+
+
+standard_scaler = StandardScaler(with_mean=True, with_std=True, copy=True)
+
+data=[[1, 3, 500, 0.5],[36, 34, 5000, 0.5]]
+#Fittedscaler = standard_scaler.fit(data)
+data_scaled=standard_scaler.fit_transform(data),
+print(data_scaled)
+
+
+
+ciao = tf.constant([data_scaled], dtype=tf.float32)
+aaa = model(ciao)
+print(aaa)
+
+
+
+
+
 # st_in = np.linspace(1, 12, 12)
 # st_out = np.linspace(2, 13, 12)
 # beta = np.linspace(0, 0.2, 10)
