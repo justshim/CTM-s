72fe7425967259d85a8cfa6b019757f16e6f56d6
Adriano <adriano.cotta.ramusino@gmail.com>
2022-09-01 17:40:10 +0200

    New database

---
+++ model/optimization/db_manip.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+path_data = "../../data/opti_data_i-j-delta-beta.CSV"
+path_cells = "../../../../CTMs-identification/fnc/extracted_data/CTM_param_out_nice.xls"
+
+data = pd.read_csv(path_data)
+cells = pd.read_excel(path_cells, sheet_name='Cells parameters')
+cells = cells.drop('t', axis=1)
+cells = cells.rename(columns={'ID': 'i'})
+
+final = pd.merge(data, cells, on='i', how='left')
+cells = cells.rename(columns={'i': 'j'})
+final = pd.merge(final, cells, on='j', how='left')
+
+final.to_csv("opti_data_all.csv", index=False, float_format='%g', encoding="utf-8")
+
+
