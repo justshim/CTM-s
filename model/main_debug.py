9f042dc43b2a85c15ecc11c1f0abf12e70f77a33
davide-unipv <62263184+davide-unipv@users.noreply.github.com>
2022-09-14 12:16:19 +0200

    modificato main_debug con le nuove phi input

--- model/main_debug.py
+++ model/main_debug.py
@@ -20,34 +20,20 @@ wb = xlrd.open_workbook(loc)
 sh = wb.sheet_by_name("Cells parameters")
 sh.cell_value(0,0)
 
-## read phi first cell from xls file
+# phi_1_24h_doublepeak
+# phi_1_24h_real
+# phi_1_24h_realsmooth
+# phi_1_24h_realsmooth_incr
+# phi_1_24h_singlepeak
 
-#loc_phi = ("C:/A_Tesi/Python/CTM-s/phi_1.xls")
-#loc_phi = ("C:/A_Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out.xls")
-#loc_phi = ("C:/Users/adria/Documents/Uni/LM II anno/Tesi/CTMs-identification/fnc/extracted_data/CTM_param_out.xls")
-#loc_phi = ("C:/Users/adria/Documents/Uni/LM II anno/Tesi/python/CTM-s/phi_1.xls")
-loc_phi=loc
-wb_phi = xlrd.open_workbook(loc_phi)
-
-#sh_phi = wb_phi.sheet_by_index(0) 		#sheet 0 is a "realistic" input (24h)
-#sh_phi = wb_phi.sheet_by_index(1)		#sheet 1 is a "synthetic" input with 2 equal peaks (24h)
-#sh_phi = wb_phi.sheet_by_index(2)		#sheet 2 is a "synthetic" input with 1 peak (24h)
-#sh_phi = wb_phi.sheet_by_index(3)		#sheet 3 is a "synthetic" input with 1 peak (3h)
-#sh_phi = wb_phi.sheet_by_index(4)		#sheet 4 is a flat input (24h)
-#sh_phi = wb_phi.sheet_by_index(5)		#sheet 5 is a real input with peak at 1800 veh/h (24h)
-#sh_phi = wb_phi.sheet_by_index(6)		#sheet 6 is a real input with peak at 2500 veh/h (24h)
-
-sh_phi = wb_phi.sheet_by_name("First Demand Real")
-sh_phi.cell_value(0,0)
+path_phi = "C:\A_Tesi\Aimsun\CTM-s\phi_input\phi_1_24h_doublepeak.txt"
 phi_zero=[]
-for i in range(0, sh_phi.nrows):
-	phi_zero.append(sh_phi.cell_value(i,0))
-
-sh_last_phi = wb.sheet_by_name("Last Demand Real")
-sh_last_phi.cell_value(0,0)
 last_phi=[]
-for i in range(0, sh_last_phi.nrows):
-	last_phi.append(sh_last_phi.cell_value(i,0))
+with open(path_phi, encoding='utf8') as fa:
+	for line in fa:
+		phi_zero.append(float(line.strip()))
+		last_phi.append(float(line.strip()))
+
 
 ###################################################
 # Initialization of all components of the model:  #
@@ -217,94 +203,79 @@ plt.plot(r0)
 plt.figure(1)
 plt.grid(True)
 plt.xlabel('k')
-plt.ylabel('rho')
-plt.plot(r9)
-plt.plot(r10)
+plt.ylabel('r1')
+plt.plot(r1)
 
 plt.figure(2)
 plt.grid(True)
 plt.xlabel('k')
-plt.ylabel('v10')
-plt.plot(v10)
+plt.ylabel('r2')
+plt.plot(r2)
 
 plt.figure(3)
+plt.grid(True)
+plt.xlabel('k')
+plt.ylabel('r3')
+plt.plot(r3)
+
+plt.figure(4)
+plt.grid(True)
+plt.xlabel('k')
+plt.ylabel('r4')
+plt.plot(r4)
+
+plt.figure(5)
+plt.grid(True)
+plt.xlabel('k')
+plt.ylabel('r5')
+plt.plot(r5)
+
+plt.figure(6)
+plt.grid(True)
+plt.xlabel('k')
+plt.ylabel('r6')
+plt.plot(r6)
+
+plt.figure(7)
+plt.grid(True)
+plt.xlabel('k')
+plt.ylabel('r7')
+plt.plot(r7)
+
+plt.figure(8)
+plt.grid(True)
+plt.xlabel('k')
+plt.ylabel('r8')
+plt.plot(r8)
+
+plt.figure(9)
+plt.grid(True)
+plt.xlabel('k')
+plt.ylabel('r9')
+plt.plot(r9)
+
+plt.figure(10)
+plt.grid(True)
+plt.xlabel('k')
+plt.ylabel('r10')
+plt.plot(r10)
+
+plt.figure(11)
 plt.grid(True)
 plt.xlabel('k')
 plt.ylabel('r12')
 plt.plot(r12)
 
-# plt.figure(4)
-# plt.grid(True)
-# plt.xlabel('k')
-# plt.ylabel('cong0')
-# plt.plot(cong0)
-
-# #plt.figure(5)
-# plt.grid(True)
-# plt.xlabel('k')
-# plt.ylabel('cong1')
-# plt.plot(cong1)
-
-plt.figure(6)
-plt.grid(True)
-plt.xlabel('k')
-plt.ylabel('congestion')
-plt.plot(cong9)
-plt.plot(cong10)
-
-plt.figure(7)
-plt.grid(True)
-plt.xlabel('k')
-plt.plot(demand)
-plt.plot(demand_next)
-plt.legend(['demand','demand_next'], title = "Legend")
-
-plt.figure(77)
-plt.grid(True)
-plt.xlabel('k')
-plt.plot(s0)
-plt.plot(s1)
-plt.plot(s2)
-plt.plot(s3)
-plt.plot(s4)
-plt.plot(s5)
-plt.plot(s6)
-plt.plot(s7)
-plt.plot(s8)
-plt.plot(s9)
-plt.plot(s10)
-plt.plot(s11)
-plt.plot(s12)
-plt.legend(['s0','s1','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11','s12'], title = "Legend")
-
-plt.figure(8)
+plt.figure(90)
 plt.grid(True)
 plt.xlabel('k')
 plt.ylabel('fipiu')
 plt.plot(fipiu)
 
-
+plt.figure(91)
 plt.grid(True)
 plt.xlabel('k')
 plt.ylabel('fimeno')
 plt.plot(fimeno)
-
-
-# plt.figure(7)
-# plt.grid(True)
-# plt.xlabel('k')
-# plt.ylabel('cong10')
-# plt.plot(cong10)
-
-# plt.figure(99)
-# plt.grid(True)
-# plt.xlabel('k')
-# plt.ylabel('cong11')
-# plt.plot(cong11)
-
-# plt.figure(99)
-# plt.grid(True)
-# plt.plot(phi_zero)
-# plt.scatter(np.linspace(0, 8640, 8640), l0, 4, marker="x")
 
 plt.show()
