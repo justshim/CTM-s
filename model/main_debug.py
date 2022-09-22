29b86b1050c887403218fa9baa3c7f3d981af5f3
davide-unipv <62263184+davide-unipv@users.noreply.github.com>
2022-09-22 17:41:28 +0200

    cvs density - flow - velocity

--- model/main_debug.py
+++ model/main_debug.py
@@ -1,3 +1,5 @@
+import csv
+
 from model import cell as c
 from model import supervisor as s
 from model import factory as f
@@ -29,10 +31,12 @@ sh.cell_value(0,0)
 path_phi = "C:\A_Tesi\Aimsun\CTM-s\phi_input\phi_1_24h_realsmooth_incr.txt"
 phi_zero=[]
 last_phi=[]
+summma = 0
 with open(path_phi, encoding='utf8') as fa:
 	for line in fa:
 		phi_zero.append(float(line.strip()))
 		last_phi.append(float(line.strip()))
+		summma=float(line.strip()) + summma
 
 
 ###################################################
@@ -44,7 +48,7 @@ fac = f.Factory()
 
 ## create the stretch via the factory
 		 		#timeLength [h],   lastPhi, 	phi_zero
-fac.createStretch(sh.cell_value(2,6), last_phi, phi_zero) 
+fac.createStretch(sh.cell_value(2,6), last_phi, phi_zero)
 
 ## create the cells via the factory
 for i in range(1, sh.nrows):
@@ -53,7 +57,7 @@ for i in range(1, sh.nrows):
 		
 ## create the stations via the factory
 			#ID stretch, r_s_max, i, j, delta, beta_s, p
-fac.addStationToStretch(0, 500, 8, 10, 240, 0.07, 0.05)
+fac.addStationToStretch(0, 1500, 8, 10, 144, 0.07, 0.05)
 
 ## create the on-ramps via the factory
 			#ID stretch, d_r, r_r_max, j, p_r
@@ -83,21 +87,33 @@ r10 = []
 r11 = []
 r12 = []
 
-s0 = []
-s1 = []
-s2 = []
-s3 = []
-s4 = []
-s5 = []
-s6 = []
-s7 = []
-s8 = []
-s9 = []
-s10 = []
-s11 = []
-s12 = []
+phi0 = []
+phi1 = []
+phi2 = []
+phi3 = []
+phi4 = []
+phi5 = []
+phi6 = []
+phi7 = []
+phi8 = []
+phi9 = []
+phi10 = []
+phi11 = []
+phi12 = []
 
+v0 = []
+v1 = []
+v2 = []
+v3 = []
+v4 = []
+v5 = []
+v6 = []
+v7 = []
+v8 = []
+v9 = []
 v10 = []
+v11 = []
+v12 = []
 
 cong0 = []
 cong1 = []
@@ -119,7 +135,9 @@ demand_next = []
 supply_next = []
 fipiu = []
 fimeno = []
+fitot = 0
 phi = []
+
 ##################################
 # Exectution of the simulation:  #
 ##################################
@@ -131,67 +149,94 @@ while k<8640: 	# k=24h=8640 , k=1h=360, k=3h=1080
 	fac.stretches[0].update(k)
 	k = k + 1
 	# save the various parameters in the previously created variables
-	l0.append(fac.stretches[0].stations[0].l[k])
-	e0.append(fac.stretches[0].stations[0].e[k])
+	# l0.append(fac.stretches[0].stations[0].l[k])
+	# e0.append(fac.stretches[0].stations[0].e[k])
+	#
+	# r0.append(fac.stretches[0].cells[0].rho[k])
+	# r1.append(fac.stretches[0].cells[1].rho[k])
+	# r2.append(fac.stretches[0].cells[2].rho[k])
+	# r3.append(fac.stretches[0].cells[3].rho[k])
+	# r4.append(fac.stretches[0].cells[4].rho[k])
+	# r5.append(fac.stretches[0].cells[5].rho[k])
+	# r6.append(fac.stretches[0].cells[6].rho[k])
+	# r7.append(fac.stretches[0].cells[7].rho[k])
+	# r8.append(fac.stretches[0].cells[8].rho[k])
+	# r9.append(fac.stretches[0].cells[9].rho[k])
+	# r10.append(fac.stretches[0].cells[10].rho[k])
+	# r11.append(fac.stretches[0].cells[11].rho[k])
+	# r12.append(fac.stretches[0].cells[12].rho[k])
+	#
+	phi0.append(fac.stretches[0].cells[0].phi)
+	phi1.append(fac.stretches[0].cells[1].phi)
+	phi2.append(fac.stretches[0].cells[2].phi)
+	phi3.append(fac.stretches[0].cells[3].phi)
+	phi4.append(fac.stretches[0].cells[4].phi)
+	phi5.append(fac.stretches[0].cells[5].phi)
+	phi6.append(fac.stretches[0].cells[6].phi)
+	phi7.append(fac.stretches[0].cells[7].phi)
+	phi8.append(fac.stretches[0].cells[8].phi)
+	phi9.append(fac.stretches[0].cells[9].phi)
+	phi10.append(fac.stretches[0].cells[10].phi)
+	phi11.append(fac.stretches[0].cells[11].phi)
+	phi12.append(fac.stretches[0].cells[12].phi)
+	#
+	# phi.append(fac.stretches[0].cells[9].phi)
+	#
+	# demand.append(fac.stretches[0].cells[9].d_big)
+	# supply.append(fac.stretches[0].cells[9].s_big)
+	# demand_next.append(fac.stretches[0].cells[10].d_big)
+	# supply_next.append(fac.stretches[0].cells[10].s_big)
+	#
+	# fipiu.append(fac.stretches[0].cells[10].phi_plus)
+	fimeno.append(fac.stretches[0].cells[12].phi)
+	fitot = fitot + fac.stretches[0].cells[12].phi
 
-	r0.append(fac.stretches[0].cells[0].rho[k])
-	r1.append(fac.stretches[0].cells[1].rho[k])
-	r2.append(fac.stretches[0].cells[2].rho[k])
-	r3.append(fac.stretches[0].cells[3].rho[k])
-	r4.append(fac.stretches[0].cells[4].rho[k])
-	r5.append(fac.stretches[0].cells[5].rho[k])
-	r6.append(fac.stretches[0].cells[6].rho[k])
-	r7.append(fac.stretches[0].cells[7].rho[k])
-	r8.append(fac.stretches[0].cells[8].rho[k])
-	r9.append(fac.stretches[0].cells[9].rho[k])
-	r10.append(fac.stretches[0].cells[10].rho[k])
-	r11.append(fac.stretches[0].cells[11].rho[k])
-	r12.append(fac.stretches[0].cells[12].rho[k])
+	# ss = fac.stretches[0].stations[0].s_s
+	# v0.append(fac.stretches[0].cells[0].v[k-1])
+	# v1.append(fac.stretches[0].cells[1].v[k-1])
+	# v2.append(fac.stretches[0].cells[2].v[k-1])
+	# v3.append(fac.stretches[0].cells[3].v[k-1])
+	# v4.append(fac.stretches[0].cells[4].v[k-1])
+	# v5.append(fac.stretches[0].cells[5].v[k-1])
+	# v6.append(fac.stretches[0].cells[6].v[k-1])
+	# v7.append(fac.stretches[0].cells[7].v[k-1])
+	# v8.append(fac.stretches[0].cells[8].v[k-1])
+	# v9.append(fac.stretches[0].cells[9].v[k - 1])
+	# v10.append(fac.stretches[0].cells[10].v[k - 1])
+	# v11.append(fac.stretches[0].cells[11].v[k - 1])
+	# v12.append(fac.stretches[0].cells[12].v[k - 1])
 
-	s0.append(fac.stretches[0].cells[0].s_big)
-	s1.append(fac.stretches[0].cells[1].s_big)
-	s2.append(fac.stretches[0].cells[2].s_big)
-	s3.append(fac.stretches[0].cells[3].s_big)
-	s4.append(fac.stretches[0].cells[4].s_big)
-	s5.append(fac.stretches[0].cells[5].s_big)
-	s6.append(fac.stretches[0].cells[6].s_big)
-	s7.append(fac.stretches[0].cells[7].s_big)
-	s8.append(fac.stretches[0].cells[8].s_big)
-	s9.append(fac.stretches[0].cells[9].s_big)
-	s10.append(fac.stretches[0].cells[10].s_big)
-	s11.append(fac.stretches[0].cells[11].s_big)
-	s12.append(fac.stretches[0].cells[12].s_big)
-
-	phi.append(fac.stretches[0].cells[9].phi)
-
-	demand.append(fac.stretches[0].cells[9].d_big)
-	supply.append(fac.stretches[0].cells[9].s_big)
-	demand_next.append(fac.stretches[0].cells[10].d_big)
-	supply_next.append(fac.stretches[0].cells[10].s_big)
-
-	fipiu.append(fac.stretches[0].cells[10].phi_plus)
-	fimeno.append(fac.stretches[0].cells[10].phi_minus)
-
-	v10.append(fac.stretches[0].cells[10].v[k-1])
-
-	cong0.append(fac.stretches[0].cells[0].congestion_state)
-	cong1.append(fac.stretches[0].cells[1].congestion_state)
-	cong2.append(fac.stretches[0].cells[2].congestion_state)
-	cong3.append(fac.stretches[0].cells[3].congestion_state)
-	cong4.append(fac.stretches[0].cells[4].congestion_state)
-	cong5.append(fac.stretches[0].cells[5].congestion_state)
-	cong6.append(fac.stretches[0].cells[6].congestion_state)
-	cong7.append(fac.stretches[0].cells[7].congestion_state)
-	cong8.append(fac.stretches[0].cells[8].congestion_state)
-	cong9.append(fac.stretches[0].cells[9].congestion_state)
-	cong10.append(fac.stretches[0].cells[10].congestion_state)
-	cong11.append(fac.stretches[0].cells[11].congestion_state)
-
-	d.append(fac.stretches[0].delta_big[k-1])
+	# cong0.append(fac.stretches[0].cells[0].congestion_state)
+	# cong1.append(fac.stretches[0].cells[1].congestion_state)
+	# cong2.append(fac.stretches[0].cells[2].congestion_state)
+	# cong3.append(fac.stretches[0].cells[3].congestion_state)
+	# cong4.append(fac.stretches[0].cells[4].congestion_state)
+	# cong5.append(fac.stretches[0].cells[5].congestion_state)
+	# cong6.append(fac.stretches[0].cells[6].congestion_state)
+	# cong7.append(fac.stretches[0].cells[7].congestion_state)
+	# cong8.append(fac.stretches[0].cells[8].congestion_state)
+	# cong9.append(fac.stretches[0].cells[9].congestion_state)
+	# cong10.append(fac.stretches[0].cells[10].congestion_state)
+	# cong11.append(fac.stretches[0].cells[11].congestion_state)
+	#
+	# d.append(fac.stretches[0].delta_big[k-1])
 
 print("\nEnd")
-	
+print("einvece")
+print(fitot/8640)
 #print("Len rho: " + str(len(fac.stretches[0].cells[0].rho))) 
+
+
+with open("./flow.csv", 'w', encoding='UTF8', newline='') as f_out:
+	writer = csv.writer(f_out, delimiter=';')
+	header = ["phi0", "phi1", "phi2", "phi3", "phi4", "phi5","phi6", "phi7", "phi8","phi9", "phi10", "phi11","phi12"]
+	writer.writerow(header)
+	for i in range(1, len(phi0)):
+		writer.writerow([phi0[i], phi1[i],phi2[i], phi3[i], phi4[i], phi5[i],phi6[i], phi7[i],phi8[i], phi9[i],phi10[i], phi11[i],phi12[i]])
+
+
+
+
 
 #############################
 # Plot management section:  #
@@ -200,103 +245,103 @@ x_time = np.linspace(1, 24, 8640)
 plt.figure(0)
 plt.grid(True)
 plt.xlabel('time [h]')
-plt.ylabel('flow')
-plt.plot(x_time, phi)
-
-plt.figure(1)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('r1')
-plt.plot(x_time, r1)
-
-plt.figure(2)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('r2')
-plt.plot(x_time, r2)
-
-plt.figure(3)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('r3')
-plt.plot(x_time, r3)
-
-plt.figure(4)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('r4')
-plt.plot(x_time, r4)
-
-plt.figure(5)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('r5')
-plt.plot(x_time, r5)
-
-plt.figure(6)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('r6')
-plt.plot(x_time, r6)
-
-plt.figure(7)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('r7')
-plt.plot(x_time, r7)
-
-plt.figure(8)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('r8')
-plt.plot(x_time, r8)
-
-plt.figure(9)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('r9')
-plt.plot(x_time, r9)
-
-plt.figure(10)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('r10')
-plt.plot(x_time, r10)
-
-plt.figure(11)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('r11')
-plt.plot(x_time, r11)
-
-plt.figure(12)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('r12')
-plt.plot(x_time, r12)
-
-plt.figure(90)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('fipiu')
-plt.plot(x_time, fipiu)
-
-plt.figure(91)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('fimeno')
+plt.ylabel('phi-')
 plt.plot(x_time, fimeno)
-
-plt.figure(80)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('e')
-plt.plot(x_time, e0)
-
-plt.figure(81)
-plt.grid(True)
-plt.xlabel('time [h]')
-plt.ylabel('l')
-plt.plot(x_time, l0)
-
+#
+# plt.figure(1)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('r1')
+# plt.plot(x_time, r1)
+#
+# plt.figure(2)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('r2')
+# plt.plot(x_time, r2)
+#
+# plt.figure(3)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('r3')
+# plt.plot(x_time, r3)
+#
+# plt.figure(4)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('r4')
+# plt.plot(x_time, r4)
+#
+# plt.figure(5)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('r5')
+# plt.plot(x_time, r5)
+#
+# plt.figure(6)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('r6')
+# plt.plot(x_time, r6)
+#
+# plt.figure(7)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('r7')
+# plt.plot(x_time, r7)
+#
+# plt.figure(8)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('r8')
+# plt.plot(x_time, r8)
+#
+# plt.figure(9)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('r9')
+# plt.plot(x_time, r9)
+#
+# plt.figure(10)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('r10')
+# plt.plot(x_time, r10)
+#
+# plt.figure(11)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('r11')
+# plt.plot(x_time, r11)
+#
+# plt.figure(12)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('s11')
+# plt.plot(x_time, s11)
+#
+# plt.figure(90)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('fipiu')
+# plt.plot(x_time, fipiu)
+#
+# plt.figure(91)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('fimeno')
+# plt.plot(x_time, fimeno)
+#
+# plt.figure(80)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('e')
+# plt.plot(x_time, e0)
+#
+# plt.figure(81)
+# plt.grid(True)
+# plt.xlabel('time [h]')
+# plt.ylabel('l')
+# plt.plot(x_time, l0)
+#
 plt.show()
