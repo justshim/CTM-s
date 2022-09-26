b08903390b05d7eebbdf001e66779ea6f93dedaf
adriano-cr <adriano.cotta.ramusino@gmail.com>
2022-09-26 11:29:49 +0200

    New NN ma stavolta funziona

--- model/optimization/NNv2.py
+++ model/optimization/NNv2.py
@@ -11,67 +11,61 @@ from tensorflow.keras.layers import Dense, Dropout
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.losses import *
 
+INPUT_PATH = '../../data/input_all.csv'
+OUTPUT_PATH = '../../data/output_all.csv'
 
-TRAIN_DATA_PATH = '../../data/opti_data_all_priority.csv'
-TEST_DATA_PATH = '../../data/opti_data_all_validazione.csv'
-
-TARGET_NAME_1 = 'integral'
-TARGET_NAME_2 = 'pi'
 print("GPU test: " + str(tf.test.is_gpu_available()) + "\n")
-# x_train = features, y_train = target
-train_data = pd.read_csv(TRAIN_DATA_PATH)
-train_data = train_data.drop(['max_delta'], axis=1)
 
-test_data = pd.read_csv(TEST_DATA_PATH)
-test_data = test_data.drop(['max_delta'], axis=1)
+x_train = pd.read_csv(INPUT_PATH, sep=";", header=None)
+y_train = pd.read_csv(OUTPUT_PATH, sep=";", header=None)
 
-y_train = train_data[[TARGET_NAME_1, TARGET_NAME_2]]
-y_test = test_data[[TARGET_NAME_1, TARGET_NAME_2]]
-#print(y_test)
+#print(x_train)
+#print(y_train)
 
-y_train['pi']=y_train['pi']*100
-y_test['pi']=y_test['pi']*100
 
-x_train = train_data.drop([TARGET_NAME_1, TARGET_NAME_2], axis=1)
-x_test = test_data.drop([TARGET_NAME_1, TARGET_NAME_2], axis=1)
-
-print(x_train)
-#print(y_test)
-def scale_datasets(x_train, x_test):
+def scale_datasets(x_t):
     # Standard Scale test and train data
     # Z - Score normalization
     standard_scaler = StandardScaler()
-    x_train_scaled = pd.DataFrame(
-        standard_scaler.fit_transform(x_train),
-        columns=x_train.columns
+    x_t_s = pd.DataFrame(
+        standard_scaler.fit_transform(x_t),
+        columns=x_t.columns
     )
-    x_test_scaled = pd.DataFrame(
-        standard_scaler.fit_transform(x_test),
-        columns=x_test.columns
-    )
-    return x_train_scaled, x_test_scaled
+    return x_t_s
 
 
-x_train_scaled, x_test_scaled = scale_datasets(x_train, x_test)
+x_train_scaled = scale_datasets(x_train)
 
-hidden_units1 = 15
+hidden_units1 = 150
 hidden_units2 = 10
 hidden_units3 = 150
-learning_rate = 0.1
+learning_rate = 0.001
+
+
+def input_flatten(df):
+    output = pd.DataFrame()
+    for index in range(0, len(df.index), 15):
+        matrix = df.loc[index:index + 14, :].to_numpy()
+        flat = matrix.flatten()
+        aa = pd.DataFrame(flat).transpose()
+        output = pd.concat([output, aa])
+    return output
+
+input = input_flatten(x_train_scaled)
 
 
 # Creating model using the Sequential in tensorflow
 # Dense = fully connected layer, with num of outputs; Dropout helps avoid overfitting
 def build_model_using_sequential():
     mdl = Sequential([
-        Dense(15, input_shape=(15,), activation='relu'),
+        Dense(90, input_shape=(90,), activation='relu'),
         Dense(hidden_units1, kernel_initializer='he_normal', activation='relu'),
         Dropout(0.2),
-        Dense(hidden_units2, kernel_initializer='he_normal', activation='relu'),
-        Dropout(0.2),
-        #Dense(hidden_units3, kernel_initializer='he_normal', activation='relu'),
-        #Dropout(0.2),
-        Dense(2)
+        # Dense(hidden_units2, kernel_initializer='he_normal', activation='relu'),
+        # Dropout(0.2),
+        # Dense(hidden_units3, kernel_initializer='he_normal', activation='relu'),
+        # Dropout(0.2),
+        Dense(3)
     ])
     return mdl
 
@@ -87,37 +81,27 @@ mse = MeanSquaredError()
 # Adam is a stochastic gradient descent method based on adaptive estimation of first-order and second-order moments
 model.compile(
     loss=msle,
-    #optimizer=Adam(learning_rate=learning_rate)
+    # optimizer=Adam(learning_rate=learning_rate)
     optimizer=Adam(),
-    metrics=['accuracy']
+    metrics=[msle]
 )
 # train the model
 history = model.fit(
-    x_train_scaled.values,
+    input,
     y_train.values,
-    epochs=60,
+    epochs=100,
     batch_size=256,
-    #validation_data=(x_test_scaled, y_test),
+    # validation_data=(x_test_scaled, y_test),
     validation_split=0.2
 )
 
-
-#output_predicted = model.predict(x_test_scaled.values)
+# output_predicted = model.predict(x_test_scaled.values)
 
 model.save('SavedModel/modellovdue', save_format="h5")
 
+
 def plot_history(history):
     plt.figure(1)
-    plt.subplot(211)
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'val'], loc='upper left')
-    plt.grid(True)
-
-    plt.subplot(212)
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
     plt.title('model loss')
